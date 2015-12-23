0313dd510e290cff6e87fc9549074ba37b10c204
Felix Biessmann <felix.biessmann@tu-berlin.de>
2015-12-23 21:58:51 +0100

    Web frontend done, with static html files.

--- api.py
+++ api.py
@@ -2,11 +2,8 @@
 import flask
 from flask import Flask, request, jsonify, render_template
 import os
-app = Flask(__name__, static_url_path='/web/')
+app = Flask(__name__)#, static_url_path='/web/')
 
-#from raven.contrib.flask import Sentry
-#app.config['SENTRY_DSN'] = 'https://85db503b6be040a2942ffb3a24577b14:ab5fe479abe540f4817f2e13e4ed6e95@app.getsentry.com/49850'
-#sentry = Sentry(app)
 
 from classifier import Classifier
 # from downloader import Downloader
@@ -43,20 +40,17 @@ def predict():
     if request.form.has_key('url'):
         url = request.form['url']
         title,text = fetch_url(url)
+        print title
+        print text
         return jsonify(classifier.predict(text))
     else:
         text = request.form['text']
         return jsonify(classifier.predict(text))
 
-@app.route('/api/distances.json')
-def news():
-  return flask.send_from_directory('manifesto', 'distances.json')
-
-# static files from web/build
-
+# static files 
 @app.route('/')
 def root():
-  return app.send_static_file('webInterface.html')
+  return app.send_static_file('index.html')
 
 @app.route('/<path:path>')
 def static_proxy(path):
@@ -65,7 +59,7 @@ def static_proxy(path):
 
 if __name__ == "__main__":
     port = 5000
-    classifier = Classifier(folder='manifestoproject',train=True)
+    classifier = Classifier(folder='manifestoproject',train=False)
     # Open a web browser pointing at the app.
-    os.system("open http://localhost:{0}".format(port))
+    os.system("open http://localhost:{0}/".format(port))
     app.run(host='0.0.0.0', port = port, debug = DEBUG)
